2af2831c490c0d4f2fe768032fce37f24fc8ca71
svenpersijn <149477435+svenpersijn@users.noreply.github.com>
2024-02-28 11:53:15 +0100

    Update RemoveCipheredLSPlugin.js

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -3,14 +3,17 @@ var now = new Date();
 now.setTime(now.getTime() + 1 * 3600 * 1000);
 // Override existing openDatabase to delete and recreate it on Error
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
-document.cookie = "NewDatabaseIsCreated=0; expires=" + now.toUTCString() + "; path=/";
+document.cookie = "NewDatabaseIsCreated=A; expires=" + now.toUTCString() + "; path=/";
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
-
+    document.cookie = "NewDatabaseIsCreated=B; expires=" + now.toUTCString() + "; path=/";
     return originalOpenDatabase.call(window.sqlitePlugin, options, successCallback, function() {
+	    document.cookie = "NewDatabaseIsCreated=C; expires=" + now.toUTCString() + "; path=/";
 	    sqlitePlugin.deleteDatabase(options, function() {
+		    document.cookie = "NewDatabaseIsCreated=D; expires=" + now.toUTCString() + "; path=/";
 		    window.sqlitePlugin.openDatabase(options, successCallback, errorCallback);
 		    document.cookie = "NewDatabaseIsCreated=1; expires=" + now.toUTCString() + "; path=/";
 	    }, function() {
+		    document.cookie = "NewDatabaseIsCreated=E; expires=" + now.toUTCString() + "; path=/";
 		    errorCallback();
 	    });
     });
