2681cfa85f16720a637d9f5e6d5ab55791a14b54
svenpersijn <149477435+svenpersijn@users.noreply.github.com>
2024-03-04 12:25:07 +0100

    Remove cookie

--- www/RemoveCipheredLSPlugin.js
+++ www/RemoveCipheredLSPlugin.js
@@ -1,8 +1,14 @@
 // Variable to check whether the database is deleted
 var now = new Date();
+var past = new Date();
+
 now.setTime(now.getTime() + 1 * 3600 * 1000);
+now.setTime(now.getTime() - 1 * 3600 * 1000);
 // Override existing openDatabase to delete and recreate it on Error
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
+
+// First remove old cookie
+document.cookie = "NewDatabaseIsCreated=; expires=" + past.toUTCString() + "; path=/";
 
 window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
     return originalOpenDatabase.call(window.sqlitePlugin, options, successCallback, function() {
